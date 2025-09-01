c6ab5ad520928ef8f369ebc353ad2a7d0b7b3927
owenchou1 <owen.chou@hyweb.com.tw>
2025-09-01 18:24:36 +0800

    first vuetify

--- scooter-frontend/src/App.vue
+++ scooter-frontend/src/App.vue
@@ -1,26 +1,27 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+    <template>
+      <!-- v-app 是 Vuetify 應用的根元件，提供了一個基礎的 App 結構 -->
+      <v-app>
+        <!-- v-main 是放置主要內容的地方 -->
+        <v-main>
+          <ScooterList />
+        </v-main>
+      </v-app>
+    </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+    <script>
+    import ScooterList from './components/ScooterList.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    export default {
+      name: 'App',
+      components: {
+        ScooterList
+      }
+    }
+    </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <style>
+    body {
+      font-family: 'Roboto', sans-serif; /* Vuetify 推薦的字體 */
+    }
+    </style>
+    
