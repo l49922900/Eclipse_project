f9fcbf49c55b464f1941c153a193f56c05603c5e
owenchou1 <owen.chou@hyweb.com.tw>
2025-09-09 15:42:06 +0800

    UserDto

---
+++ springboot-scooters-rental/src/main/java/com/example/demo/model/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+	private Long id;
+    private String username;
+    private String email;
+    private String role;
+    private boolean enabled;
+}
